da08db8785e44fa17667423d8487b68df06375ba
Fernando Stecconi <nandilugio@gmail.com>
2023-09-19 19:53:32 +0200

    wip [ci skip]

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import curses
 import random
 import signal
 
-from paragraph_exercise import run_paragraph_exercise
+from paragraph_state import ParagraphState
 
 
 def build_numeric_exercise(num_lines=3, num_words_per_line=4, num_digits=4):
@@ -18,6 +18,89 @@ def build_numeric_exercise(num_lines=3, num_words_per_line=4, num_digits=4):
 def build_file_exercise():
     with open('exercise.txt', 'r') as f:
         return f.read()
+
+
+def run_paragraph_exercise(win, exercise_txt):
+    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_RED)
+
+    COLORS_BY_STATE = {
+        ParagraphState.CHAR_PENDING: curses.color_pair(1),
+        ParagraphState.CHAR_CORRECT: curses.color_pair(2),
+        ParagraphState.CHAR_AMENDED: curses.color_pair(3),
+        ParagraphState.CHAR_WRONG: curses.color_pair(4),
+    }
+
+    # Draw the initial state of the screen
+    # TODO: Wrap text by words (not by characters)
+    win.clear()
+    win.addstr(exercise_txt, COLORS_BY_STATE[ParagraphState.CHAR_PENDING])
+    win.move(0,0)
+
+    state = ParagraphState(exercise_txt)
+
+    # Loop to handle keypresses
+    while True:
+        char = win.get_wch()
+
+        # Bail out if needed
+        if type(char) == int: # Keypress not mapped to a character
+            continue
+        if char == '\n':
+            continue
+
+        # Handle keypress
+        if char == '\x7f': # Backspace
+            try:
+                deleted_char = state.backspace_pressed()
+            except ParagraphState.CannotGoBack:
+                continue
+
+            # Handle the case where the cursor is at the beginning of a line
+            last_position = list(win.getyx())
+            if last_position[1] == 0:
+                last_position[0] -= 1
+                last_position[1] = win.getmaxyx()[1] - 1
+            else:
+                last_position[1] -= 1
+
+            win.addstr(*last_position, deleted_char, COLORS_BY_STATE[ParagraphState.CHAR_PENDING])
+            win.move(*last_position)
+
+        else: # Characters
+            char_state = state.char_typed(char)
+            win.addstr(char, COLORS_BY_STATE[char_state]) 
+
+        # TODO: Handle arrow keys or other special keys
+
+        if state.is_exercise_done():
+            break
+
+    # Exercise done. Move below text to display stats
+    current_position = list(win.getyx())
+    win.move(current_position[0] + 2, 0)
+
+    stats = state.get_stats()
+
+    if (stats['all_correct'] ):
+        win.addstr('All correct!')
+    else:
+        win.addstr('Errors have been made...')
+
+    win.addstr('\n')
+    win.addstr(f'\nWPM: {stats["net_wpm"]:.2f}, {stats["gross_wpm"]:.2f} gross')
+    win.addstr(f'\nAccuracy: {stats["result_accuracy"]:.2f}%, {stats["real_accuracy"]:.2f}% real')
+    win.addstr(f'\nErrors: {stats["error_count"]}, {stats["uncorrected_error_count"]} not corrected')
+    win.addstr(f'\nExcercise Length: {stats["length_txt"]} chars, {stats["length_std_words"]:.2f} "standard" words')
+    win.addstr(f'\nTime: {stats["total_time_s"]:.2f} s')
+    win.addstr('\n')
+    
+    win.refresh()
+    win.addstr('\nPress <ENTER> to continue...')
+    win.refresh()
+    win.getstr()
 
 
 # TODO: Wrap in catch-all exception handler
---
+++ paragraph_state.py
@@ -0,0 +1,85 @@
+import time
+
+
+class ParagraphState:
+    
+    class CannotGoBack(RuntimeError): pass
+
+    CHAR_PENDING = 'p'
+    CHAR_CORRECT = 'c'
+    CHAR_AMENDED = 'a'
+    CHAR_WRONG = 'w'
+
+
+    def __init__(self, exercise_txt):
+        self.exercise_txt = exercise_txt
+        
+        self.length_txt = len(self.exercise_txt)
+        self.char_state_map = list(self.CHAR_PENDING * self.length_txt)
+        self.current_char_idx = 0
+        self.error_count = 0
+        self.start_time = None
+        self.end_time = None
+
+
+    def char_typed(self, char):
+        # Start the timer if it's the first character
+        if self.start_time is None:
+            self.start_time = time.time()
+
+        # Derive the new state of the character (and count errors)
+        resulting_char_state = self.CHAR_CORRECT
+        if char == self.exercise_txt[self.current_char_idx]:
+            if self.char_state_map[self.current_char_idx] not in (self.CHAR_PENDING, self.CHAR_CORRECT):
+                resulting_char_state = self.CHAR_AMENDED
+        else:
+            resulting_char_state = self.CHAR_WRONG
+            self.error_count += 1
+
+        # Update the state of the character
+        self.char_state_map[self.current_char_idx] = resulting_char_state
+        self.current_char_idx += 1
+
+        # Stop the timer if it's the last character
+        if self.current_char_idx == self.length_txt:
+            self.end_time = time.time()
+        
+        return resulting_char_state
+
+
+    def backspace_pressed(self):
+        if self.current_char_idx == 0:
+            raise self.CannotGoBack()
+
+        self.current_char_idx -= 1
+        deleted_char = self.exercise_txt[self.current_char_idx]
+
+        return deleted_char
+    
+
+    def is_exercise_done(self):
+        return self.end_time is not None
+    
+
+    def get_stats(self):
+        length_std_words = self.length_txt / 5
+        total_time_s = self.end_time - self.start_time if self.start_time else 0
+        total_time_m = total_time_s / 60
+        uncorrected_error_count = len([x for x in self.char_state_map if x == self.CHAR_WRONG])
+        gross_wpm = length_std_words / total_time_m
+        net_wpm = gross_wpm - (uncorrected_error_count / total_time_m)
+        result_accuracy = (self.length_txt - uncorrected_error_count) * 100 / self.length_txt
+        real_accuracy = (self.length_txt - self.error_count) * 100 / self.length_txt
+
+        return {
+            'all_correct': uncorrected_error_count == 0,
+            'length_txt': self.length_txt,
+            'length_std_words': length_std_words,
+            'total_time_s': total_time_s,
+            'error_count': self.error_count,
+            'uncorrected_error_count': uncorrected_error_count,
+            'gross_wpm': gross_wpm,
+            'net_wpm': net_wpm,
+            'result_accuracy': result_accuracy,
+            'real_accuracy': real_accuracy,
+        }
