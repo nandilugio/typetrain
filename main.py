a4d083d86b1c23d4404b0d1c7faa486893ff31c6
Fernando Stecconi <nandilugio@gmail.com>
2023-09-19 21:33:16 +0200

    wip [ci skip]

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
 import curses
-import random
 import signal
 
+from exercise_builders import *
 from paragraph_state import ParagraphState
-
-
-def build_numeric_exercise(num_lines=3, num_words_per_line=4, num_digits=4):
-    exercise_txt = ''
-    for _ in range(num_lines):
-        for _ in range(num_words_per_line):
-            exercise_txt += f'{random.randint(0, 10**num_digits):0{num_digits}} '
-        exercise_txt = exercise_txt[:-1]
-        exercise_txt += '\n'
-    return exercise_txt
-
-
-def build_file_exercise():
-    with open('exercise.txt', 'r') as f:
-        return f.read()
 
 
 def update_stats_heading(win, state):
